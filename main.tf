63a0abb79413ff922d4c2307285944521e7f8997
Daniel Piddock <33028589+dpiddockcmp@users.noreply.github.com>
2019-08-12 15:22:35 +0200

    Error: subject_alternative_names: should be a list (#7)

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 resource "aws_acm_certificate" "this" {
   domain_name               = "${var.domain_name}"
   validation_method         = "DNS"
-  subject_alternative_names = "${var.subject_alternative_names}"
+  subject_alternative_names = ["${var.subject_alternative_names}"]
   tags                      = "${var.tags}"
 }
 
